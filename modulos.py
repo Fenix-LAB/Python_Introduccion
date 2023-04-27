cc975d8b0c2c2a54dadf106971917716b248eb96
Christian-RH <fenixcr.123@gmail.com>
2023-04-27 14:20:56 -0600

    Modulos final

--- modulos.py
+++ modulos.py
@@ -1,6 +1,10 @@
 #import math # Moculo local
-import math as m # Sobrenombre de un modulo
+import math as m # Sobrenombre de un modulo #include <math.h>
 #from math import * # = import math
+
+import numpy as np
+import cv2
+
 
 x = m.sqrt(4)
 print(x)
